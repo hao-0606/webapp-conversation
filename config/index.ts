179ede8e1dce8d4a0f5556e46e05783cafad103c
hao-0606 <hao0606@proton.me>
2025-11-19 19:05:22 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,11 +3,11 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
+  title: 'AI 辯論',
+  description: '丟出一個主題，讓 AI 進行辯論，並有評委進行評分',
   copyright: '',
   privacy_policy: '',
-  default_language: 'en',
+  default_language: 'zh',
   disable_session_same_site: false, // set it to true if you want to embed the chatbot in an iframe
 }
 
